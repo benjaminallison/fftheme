e06e942bd217619f74ea85e5861830e51589b592
Benjamin Allison <ben@roestudios.com>
2015-06-02 23:45:22 -0700

    no message

--- lib/template-features.php
+++ lib/template-features.php
@@ -42,24 +42,32 @@
 	add_image_size( '2x1-small', 150, 75, true );
 	add_image_size( '2x1-medium', 600, 300, true );
 	add_image_size( '2x1-large', 1200, 600, true );
+	add_image_size( '2x1-massive', 1600,800, false );
 	add_image_size( '1x2-small', 75, 150, true );
 	add_image_size( '1x2-medium', 300, 600, true );
 	add_image_size( '1x2-large', 600, 1200, true );
+	add_image_size( '1x2-massive', 800, 1600, true );
 	add_image_size( '16x9-small', 150, 84, true );
 	add_image_size( '16x9-medium', 600, 336, true );
 	add_image_size( '16x9-large', 1200, 675, true );
+	add_image_size( '16x9-large', 1200, 675, true );
+	add_image_size( '16x9-massive', 1600, 900, true );
 	add_image_size( '4x3-small', 150, 112, true );
 	add_image_size( '4x3-medium', 600, 450, true );
 	add_image_size( '4x3-large', 1200, 675, true );
+	add_image_size( '4x3-massive', 1600, 1200, true );
 	add_image_size( '3x2-small', 150, 100, true );
 	add_image_size( '3x2-medium', 600, 400, true );
 	add_image_size( '3x2-large', 1200, 800, true );
+	add_image_size( '3x2-massive', 1600, 1066, true );
 	add_image_size( '1x1-small', 150, 150, true );
 	add_image_size( '1x1-medium', 600, 600, true );
 	add_image_size( '1x1-large', 1200, 1200, true );
+	add_image_size( '1x1-massive', 1600, 1600, true );
 	add_image_size( 'natural-small', 150, 150, false );
 	add_image_size( 'natural-medium', 600, 600, false );
 	add_image_size( 'natural-large', 1200, 1200, false );
+	add_image_size( 'natural-massive', 1600, 1600, false );
 
 	function set_permalink(){
 		global $wp_rewrite;
