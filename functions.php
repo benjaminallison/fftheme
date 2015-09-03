78e3b5d020c5b729dd240db21d0d8e52498765d0
Benjamin Allison <benjamin@fireflycompany.com>
2015-09-03 12:12:18 -0700

    no message

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 	include(dirname(__FILE__) . '/lib/template-functions.php');
 	include(dirname(__FILE__) . '/lib/template-tags.php');
 	include(dirname(__FILE__) . '/lib/template-shortcodes.php');
+	include(dirname(__FILE__) . '/lib/template-kill-garbage.php');
 	include(dirname(__FILE__) . '/lib/enqueue-styles.php');
 	include(dirname(__FILE__) . '/lib/enqueue-scripts.php');
 	include(dirname(__FILE__) . '/lib/dequeuing.php');
