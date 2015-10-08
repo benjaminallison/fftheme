8989bce44507dee6730b2aa74704b86b015bae93
Benjamin Allison <benjamin@fireflycompany.com>
2015-10-08 16:59:05 -0700

    no message

---
+++ partials/header-favicons.php
@@ -0,0 +1,5 @@
+<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
+<link rel="icon" type="image/png" href="/favicons/favicon.png" sizes="16x16">
+<link rel="shortcut icon" type="image/x-icon" href="/favicons/favicon.png">
