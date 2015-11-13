ea100148b6d251f014301ccf590dfb46afa74f57
Benjamin Allison <benjamin@fireflycompany.com>
2015-11-13 11:44:18 -0800

    no message

---
+++ function.php
@@ -0,0 +1,20 @@
+<?php
+	include(dirname(__FILE__) . '/lib/utilities.php');
+	include(dirname(__FILE__) . '/lib/excerpt-prefs.php');
+	include(dirname(__FILE__) . '/lib/template-features.php');
+	include(dirname(__FILE__) . '/lib/template-filters.php');
+	include(dirname(__FILE__) . '/lib/template-functions.php');
+	include(dirname(__FILE__) . '/lib/template-tags.php');
+	include(dirname(__FILE__) . '/lib/template-shortcodes.php');
+	include(dirname(__FILE__) . '/lib/template-kill-garbage.php');
+	include(dirname(__FILE__) . '/lib/enqueue-styles.php');
+	include(dirname(__FILE__) . '/lib/enqueue-scripts.php');
+	include(dirname(__FILE__) . '/lib/dequeuing.php');
+	include(dirname(__FILE__) . '/lib/default-setup.php');
+	include(dirname(__FILE__) . '/lib/cpt-init.php');
+	include(dirname(__FILE__) . '/lib/taxonomy-init.php');
+	include(dirname(__FILE__) . '/lib/auto-install-plugins.php');
+	include(dirname(__FILE__) . '/lib/admin-menu.php');
+	// include(dirname(__FILE__) . '/lib/images-rotate-on-upload.php');
+	// include(dirname(__FILE__) . '/lib/admin-options.php');
+	// add_filter('show_admin_bar', '__return_false');
