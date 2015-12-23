8d9f31c1af1a52e1ab60fc9a1219cf20fb0d7ee9
Benjamin Allison <benjamin@fireflycompany.com>
2015-12-23 15:43:27 -0800

    no message

--- lib/admin-options.php
+++ lib/admin-options.php
@@ -2,27 +2,7 @@
 // create custom plugin settings menu
 
 if( function_exists('acf_add_options_page') ) {
-
-	acf_add_options_page();
-
-	// acf_add_options_page(array(
-	// 	'page_title' 	=> 'Theme Options',
-	// 	'menu_title'	=> 'Theme Options',
-	// 	'menu_slug' 	=> 'theme-general-settings',
-	// 	'capability'	=> 'edit_posts',
-	// 	'redirect'		=> false
-	// ));
-	// 
-	// acf_add_options_sub_page(array(
-	// 	'page_title' 	=> 'External Links',
-	// 	'menu_title'	=> 'External Links',
-	// 	'parent_slug'	=> 'theme-general-settings',
-	// ));
-	// 
-	// acf_add_options_sub_page(array(
-	// 	'page_title' 	=> 'Socials',
-	// 	'menu_title'	=> 'Socials',
-	// 	'parent_slug'	=> 'theme-general-settings',
-	// ));
-	
+	acf_add_options_page(array(
+		'page_title' => 'Theme Options'
+	));
 }
