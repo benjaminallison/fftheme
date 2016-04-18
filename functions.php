66d4b0716509f7797efd6f24385d780c6f6ea590
Benjamin Allison <ben@roestudios.com>
2016-04-18 14:18:40 -0700

    no message

--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@
 	include(dirname(__FILE__) . '/lib/default-setup.php');
 	include(dirname(__FILE__) . '/lib/auto-install-plugins.php');
 	include(dirname(__FILE__) . '/lib/admin-menu.php');
+	include(dirname(__FILE__) . '/lib/admin-tinymce.php');
+
 	// include(dirname(__FILE__) . '/lib/images-rotate-on-upload.php');
 	// include(dirname(__FILE__) . '/lib/admin-options.php');
 	// add_filter('show_admin_bar', '__return_false');
