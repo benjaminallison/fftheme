81e2c79bb80c7ec5916c51a49c0ecf78f29d7e58
Benjamin Allison <benjamin@fireflycompany.com>
2016-07-20 16:19:33 -0700

    no message

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 			<header id="header" class="page_header">
 				<div class="container">
 					<nav id="nav" class="col_12">
-						<div class="col_5 gutter_pad header_logo">
+						<div class="col_5 gutter_pad">
 							<a href="#" class="button alignleft">Member SignUp</a>
 							<?php $leftMenuParams = array( 'theme_location' => 'left-menu', 'container' => false, 'items_wrap' => '%3$s' ) ; ?>
 							<ul id="left-menu" class="menu left-menu">
@@ -31,7 +31,7 @@
 						<a class="col_2 gutter_pad header_logo" href="<?php echo home_url(); ?>">
 							<img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" class="full-width" alt="" />
 						</a>
-						<div class="col_5 gutter_pad header_logo">
+						<div class="col_5 gutter_pad">
 							<a href="#" class="button alignright">Member Login</a>
 							<?php $rightMenuParams = array( 'theme_location' => 'right-menu', 'container' => false, 'items_wrap' => '%3$s' ) ; ?>
 							<ul id="right-menu" class="right-menu menu">
